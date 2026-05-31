b853b826daf4a660d8fe274a40b39b8396517a77
amin <amingholipoor5327@gamil.com>
2026-05-31 14:51:09 +0330

    added delete and object models

--- src/App.js
+++ src/App.js
@@ -1,38 +1,51 @@
 import "./App.css";
 import { useState } from "react";
+
 function App() {
-const[courselist , setCourselist] = useState([]);
-const[newcourse , setNewcourse] = useState("")
+  const [courselist, setCourselist] = useState([]);
+  const [newcourse, setNewcourse] = useState("");
 
+  const handleInput = (e) => {
+    setNewcourse(e.target.value);
+  };
 
-let HandelInput = (e)=>{
-  setNewcourse(e.target.value)
-}
+  const addCourse = () => {
+    if (newcourse.trim() === "") {
+      alert("لطفاً نام دوره را وارد کنید");
+      return;
+    }
 
-let Addcourse = ()=>{
-  setCourselist([...courselist , newcourse])
-}
+    const course = {
+      id: courselist.length === 0 ? 1 : courselist[courselist.length - 1].id + 1,
+      courseName: newcourse,
+      isComplete: false
+    };
+    setCourselist([...courselist, course]);
+    setNewcourse("");
+  };
+
+   const deleteCourse = (courseId) => {
+    setCourselist(courselist.filter((course) => course.id !== courseId));
+    };
+
   return (
     <div className="App">
-    <div>
-      <input
-       type="text"
-        placeholder="type..."
-           value={newcourse} 
-          onChange={HandelInput}>
-          </input>
-      <botton onClick={Addcourse}>Added</botton>
+      <div>
+        <input
+          type="text"
+          placeholder="type..."
+          value={newcourse}
+          onChange={handleInput}
+        />
+        <button onClick={addCourse}>Add Course</button>
+      </div>
 
-    </div>
-    {courselist.map((value , index)=>{
-      return(<div>
-        <h1 key={index}>{value}</h1>
-        <button>X</button>
-      </div>)
-    })}
-    <div>
-
-    </div>
+      {courselist.map((course) => (
+        <div key={course.id}>
+          <h1>{course.courseName}</h1>
+          <button onClick={() => deleteCourse(course.id)}>❌ Delete</button>
+        </div>
+      ))}
     </div>
   );
 }
